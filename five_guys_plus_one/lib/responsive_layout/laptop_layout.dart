72651403108adf68cf51f9c0aa8b0a07698a67ae
MajdIssa34 <issamajd00@gmail.com>
2024-04-01 20:34:22 +1100

    background done for pc and mobile

--- five_guys_plus_one/lib/responsive_layout/laptop_layout.dart
+++ five_guys_plus_one/lib/responsive_layout/laptop_layout.dart
@@ -8,74 +8,88 @@ class LaptopLayout extends StatelessWidget {
   final void Function() startQuiz;
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: myAppBar,
-    backgroundColor: Colors.transparent, // Set background color to transparent
-    drawer: myDrawer,
-    body: Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/background.jpg'), // Replace with your desired background image
-          fit: BoxFit.cover,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar,
+      backgroundColor:
+          Colors.transparent, // Set background color to transparent
+      drawer: myDrawer,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+                'assets/images/background4.jpg'), // Replace with your desired background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/images/Macquarie_University Logo.png',
+                width: 600,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(200, 40, 50, 20),
+                      child: Column(children: [
+                        Text(
+                          '"Any sufficiently advanced technology is indistinguishable from magic."',
+                          style: GoogleFonts.bebasNeue(
+                            textStyle: Theme.of(context).textTheme.displayLarge,
+                            fontSize: 80,
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center, // Center the text
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(-10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color.fromARGB(255, 255, 255, 255)
+                                    .withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                              ),
+                            ],
+                          ),
+                          child: TextButton(
+                            onPressed: startQuiz,
+                            style: TextButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 48, 45, 45),
+                              padding: const EdgeInsets.all(25),
+                            ),
+                            child: Text(
+                              "Start the Quiz",
+                              style: GoogleFonts.bebasNeue(
+                                textStyle:
+                                    Theme.of(context).textTheme.displayLarge,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w700,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              //alignment: Alignment.topLeft,
-              'assets/images/Macquarie_University Logo.png',
-              width: 600,
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: Text(
-                '"Any sufficiently advanced technology is indistinguishable from magic"',
-                style: GoogleFonts.bebasNeue(
-                  textStyle: Theme.of(context).textTheme.displayLarge,
-                  fontSize: 60,
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center, // Center the text
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(-10),
-                boxShadow: [
-                  BoxShadow(
-                    color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.1),
-                    spreadRadius: 1,
-                    blurRadius: 5,
-                  ),
-                ],
-              ),
-              child: TextButton(
-                onPressed: startQuiz,
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 48, 45, 45),
-                  padding: const EdgeInsets.all(25),
-                ),
-                child: Text(
-                  "Start the Quiz",
-                  style: GoogleFonts.bebasNeue(
-                  textStyle: Theme.of(context).textTheme.displayLarge,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.white,
-                ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
-}}
+    );
+  }
+}
